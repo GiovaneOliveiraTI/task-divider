bc90094c1e0d3a733a1866a1db846ef07030ed19
f550971 <f550971@corp.caixa.gov.br>
2021-03-22 17:18:07 -0300

    First commit.

---
+++ src/main/java/com/br/pos/taskdivider/TaskDividerApplication.java
@@ -0,0 +1,13 @@
+package com.br.pos.taskdivider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDividerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskDividerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/pos/taskdivider/TaskDividerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pos.taskdivider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDividerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
