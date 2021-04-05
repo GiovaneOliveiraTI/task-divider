bc15d07043068820a0dc02098a728d0ab1313fa9
f550971 <f550971@corp.caixa.gov.br>
2021-04-05 11:57:02 -0300

    Criando usuarios em memoria para testes.

--- src/main/java/com/br/pos/taskdivider/security/WebSecurityConfig.java
+++ src/main/java/com/br/pos/taskdivider/security/WebSecurityConfig.java
@@ -2,11 +2,14 @@ package com.br.pos.taskdivider.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,6 +18,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] PATHS = new String[]{"/tarefa/**", "/categoria/**", "/usuario/**"};
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -29,10 +34,34 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("usuario")
+                .password(passwordEncoder().encode("senha"))
+                .roles("USER");
     }
 
-
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .antMatchers(HttpMethod.POST, PATHS)
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, PATHS)
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, PATHS)
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, PATHS)
+                .hasAnyRole("ADMIN", "USER")
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
 
 
 }
